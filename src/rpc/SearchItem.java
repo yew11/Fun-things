620e685f17c2777f7bb172dab298c1f704cc2e9a
ywang0811 <ywang0811@gmail.com>
2018-07-30 22:25:47 -0400

    first commit upload files

---
+++ src/rpc/SearchItem.java
@@ -0,0 +1,75 @@
+package rpc;
+
+import java.util.List;
+import java.util.Set;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import db.DBConnection;
+import db.DBConnectionFactory;
+import entity.Item;
+import external.TicketMasterAPI;
+
+/**
+ * Servlet implementation class SearchItem
+ */
+@WebServlet(name = "search", urlPatterns = { "/search" })
+public class SearchItem extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchItem() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		double lat = Double.parseDouble(request.getParameter("lat"));
+		double lon = Double.parseDouble(request.getParameter("lon"));
+		String term = request.getParameter("term");
+		String userId = request.getParameter("user_id");
+
+		DBConnection connection = DBConnectionFactory.getConnection();
+		try {
+			List<Item> items = connection.searchItems(lat, lon, term);
+			Set<String> favoriteItems = connection.getFavoriteItemIds(userId);
+			
+			JSONArray array = new JSONArray();
+			for (Item item : items) {
+				JSONObject obj = item.toJSONObject();
+				obj.put("favorite", favoriteItems.contains(item.getItemId()));
+				array.put(item.toJSONObject());
+			}
+			RpcHelper.writeJsonArray(response, array);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		} finally {
+			connection.close();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
